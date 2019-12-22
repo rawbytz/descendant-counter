7c9979ae860b80b8f49c95785592ea8feb965dad
rawbytz <rawbytz@gmail.com>
2019-12-22 09:55:35 -0700

    minor refac

--- descendantCounter.js
+++ descendantCounter.js
@@ -1,9 +1,9 @@
-(function descendantCounter_1_1() {
-  const current = WF.currentItem(),
-    children = current.getChildren(),
-    pName = current.getNameInPlainText(),
-    pNum = current.getNumDescendants().toString(),
-    padMax = pNum.length;
+(function descendantCounter_1_2() {
+  const current = WF.currentItem();
+  const children = current.getChildren();
+  const pName = current.getNameInPlainText();
+  const pNum = current.getNumDescendants().toString();
+  const padMax = pNum.length;
   const counts = children.map(item => {
     let name = item.getNameInPlainText();
     if (name.length > 45) name = name.substr(0, 42) + "...";
@@ -13,4 +13,4 @@
     return `${cNum}\t${name}`
   });
   WF.showAlertDialog(`<pre><b>${pNum}\t${pName}</b><br>${counts.join("<br>")}</pre>`);
-})();
+})(); 
